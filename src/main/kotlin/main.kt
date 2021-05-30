06ec6e7b11d55637b07d5e09531c69be9cb3486d
TarunSaini063 <tsaini063@outlook.com>
2021-05-30 12:18:06 +0530

    null saftey

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,19 @@
+fun calculateLength(str : String?) : Int? {
+    return str?.length
+}
+
+fun desc(str : String?) : Int? {
+    return str?.length
+}
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var notNull: String ="I am not null"
+    var canNull : String? = "I can be null"
+    println(notNull)
+    //gives error because it is of non null type
+//    notNull = null
+    canNull = null
+    var answer = canNull?.length
+    println(calculateLength(canNull))
+    println(desc(null))
+    println(desc("null"))
 }
